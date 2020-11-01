4cae31f55dc27fbddba4011c0ac1986386ee5ab5
Joshua Vaslie <joshuavaslie2021@u.northwestern.edu>
2020-10-31 21:40:43 -0500

    logic to determine hour past/not added

--- Assets/moment.js
+++ Assets/moment.js
@@ -1,6 +1,21 @@
 var now = moment().format('LLL');
+var hourArray= [9,10,11,12,13,14,15,16,17,18,19,20,21]
 
+var hourVal = moment().hour()
+console.log(hourVal)
 
+function assignClass() {
+for (var i = 9;i < 23; i++) {
+ hourDiv.value = parseInt(hourDiv.value)
 
-console.log(now)
+if (hourVal > hourDiv.value) {
+  hourDiv.setAttribute("class","pasthour")  
 
+}
+if (hourVal < hourDiv.value) {
+    hourDiv.setAttribute("class","futurehour")
+}
+if (hourVal === hourDiv.value) {
+    hourDiv.setAttribute("class","nowhour")
+}
+}}
--- Assets/script.js
+++ Assets/script.js
@@ -4,12 +4,17 @@ var dayEl = document.getElementById("container")
 
 var workDay = ["9 am","10 am","11 am","12 pm","1 pm","2 pm","3 pm","4 pm","5 pm","6 pm","7 pm","8 pm","9 pm"]
  for (var i = 0; i < workDay.length; i++) {
-    var row = document.createElement("div")
+    var row = document.createElement('div')
     var rowLabel = document.createElement("label")
+    var hourDiv = document.createElement("div")
+    hourDiv.setAttribute("id","hourdiv")
+    hourDiv.setAttribute("value",(i + 9))
     rowLabel = workDay[i]
-    row.append(rowLabel)
+
     row.setAttribute("id",workDay[i])
     row.setAttribute("class","row")
     dayEl.appendChild(row)
-    
+    row.append(rowLabel)
+    row.append(hourDiv)
+    assignClass()
  }
